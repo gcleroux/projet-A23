952df3453c43f00ee450c76e495f54be5de21d25
Guillaume Cléroux <guillaume.cleroux@protonmail.com>
2023-12-05 10:40:41 -0500

    Added graceful shutdown for the client

--- cmd/client-cli/main.go
+++ cmd/client-cli/main.go
@@ -4,6 +4,9 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 
 	api "github.com/gcleroux/projet-A23/api/v1"
 	"github.com/gcleroux/projet-A23/src/config"
@@ -14,15 +17,20 @@ import (
 	"google.golang.org/grpc/grpclog"
 )
 
-var rootCmd = &cobra.Command{
-	Use:   "client",
-	Short: "gRPC client gateway to send REST requests to the gRPC server",
-	Run: func(cmd *cobra.Command, args []string) {
-		if err := run(); err != nil {
-			grpclog.Fatal(err)
-		}
-	},
-}
+var (
+	rootCmd = &cobra.Command{
+		Use:   "client",
+		Short: "gRPC client gateway to send REST requests to the gRPC server",
+		Run: func(cmd *cobra.Command, args []string) {
+			setupInterruptHandler()
+			if err := run(); err != nil {
+				grpclog.Fatal(err)
+			}
+		},
+	}
+
+	ctx context.Context
+)
 
 func init() {
 	if err := config.InitializeConfig(rootCmd); err != nil {
@@ -42,7 +50,7 @@ func run() error {
 		return err
 	}
 
-	ctx := context.Background()
+	ctx = context.Background()
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 
@@ -68,4 +76,19 @@ func run() error {
 	}
 
 	return http.ListenAndServe(fmt.Sprintf(":%d", conf.Client.GatewayPort), mux)
+}
+
+func setupInterruptHandler() {
+	// Set up a channel to receive interrupt signals
+	interruptChan := make(chan os.Signal, 1)
+	signal.Notify(interruptChan, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		// Wait for an interrupt signal
+		<-interruptChan
+		_, cancel := context.WithCancel(ctx)
+		cancel()
+
+		os.Exit(0)
+	}()
 }
